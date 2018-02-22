8bab62fb6300860c52be24fe253e0be3feca7679
makapuf <makapuf2@gmail.com>
2018-02-22 15:44:16 +0100

    fixes to mk_tmap

--- lib/blitter/scripts/mk_tmap.py
+++ lib/blitter/scripts/mk_tmap.py
@@ -2,20 +2,22 @@
 """
 Simple TMX to TMAP export
 
-.map format is : 
+.map format is :
     - u16 header = 0xb17b
-    - u16 map_w, 
-    - u16 map_h, 
-    - u8 map_codec 0=u8, 1=u16, ..
-    - u8 nb of layers, 
+    - u16 map_w,
+    - u16 map_h,
+    - u8 nb of layers,
+    - u8 RFU (map codec?)
 
-    - levels : N times layers
+    - levels : N times layers, map index : u16 (2bits tileset, 14 tile index, zero is always transp)
+        tilesets ids are always taken in alphabetic order. insert unused tilemaps to skip.
+        no reference to tileset in file to keep things simple
 
-    - objects : 
+    - objects : list of :
         s16 x
         s16 y
-        u8 object_id 
-        u8 value  (int(name) if name is an int or tile.property_value[name])
+        u8 object_id
+        u8 value  (int(name) if name of object is an int or tile.property_value[name])
         u8 reserved[2]
 
         record oid=255 separate object groups
@@ -38,71 +40,78 @@ from utils import abspath
 
 MAP_FORMATS = 'u16','u8'
 
-def tid2ts(tmx,id, basefile) : 
+def tid2ts(tmx,id, basefile) :
     "for a given tid in tmx, find the tileset + local tid type"
     for ts in tmx.findall('tileset'):
-        if int(ts.get('firstgid'))>id:  
+        if int(ts.get('firstgid'))>id:
             break
         last_ts=ts
 
-    rid=id-int(last_ts.get('firstgid'))
+    rid=id-int(last_ts.get('firstgid')) # local id of this tile within
     src=last_ts.get('source')
-    if src : 
+    if src :
         ts = ET.parse(abspath(basefile,src)).getroot()
     # fixme else : included in tmx
-    tile = ts.find('tile[@id="%d"]'%rid)
-    return ts.get('name'), tile.get('type') if tile is not None else None
+    return ts, rid
 
-def out_objects(tmap, basefile, of=None) : 
+def out_objects(tmap, basefile, of=None) :
     """count objects, find ids
     save values to file of.
-    returns 
-        - [('layername1',index_first_object), ('layername2',idx), ...] 
+    returns
+        - [('layername1',index_first_object), ('layername2',idx), ...]
         - [(ts,type),(ts,type),...] uniques - to spr files references
     """
     unique_oids=[]
+    unique_ts=[] # ts characteristics corresponding to unique_oids
+    unique_obj_h = [] # vertical height of tiles in this tileset in // with oids
     objgroups = []
     index=0
-    for objgroup in tmap.findall('objectgroup'): 
+    for objgroup in tmap.findall('objectgroup'):
         name=objgroup.get("name")
         if name[0]=='_' : continue # skip
         objgroups.append((name,index))
-        
+
         if of : print("#define objgroup_%s_%s %d"%(tmap_name, name, index))
         for obj in objgroup.findall('object'):
-           
+
             oid = int(obj.get('gid'))
-            if oid not in unique_oids : 
+            if oid not in unique_oids :
                 unique_oids.append(oid)
+                ts,rid = tid2ts(tmap,oid,basefile)
+                tile = ts.find('tile[@id="%d"]'%rid)
+                unique_ts.append((ts.get('name'), tile.get('type') if tile is not None else None))
+                unique_obj_h.append(int(ts.get('tileheight')))
+
+
             uid=unique_oids.index(oid)
             nm = obj.get('name')
 
-            x = int(obj.get('x'))
-            y = int(obj.get('y'))
+            x = int(float(obj.get('x')))
+            y = int(float(obj.get('y'))) # beware y is bottom of object
+            y -= unique_obj_h[uid] # remove height of this tile
             if nm and nm.startswith('_') : continue
             v = int(nm) if nm is not None and nm.isdigit() else 0
             # fixme values from names / other properties ?
-
             if of : of.write(struct.pack('2h4B',x,y,uid,v,0,0)) ; index +=1
-            # verify ids from all tsx + local, find sprite .. 
+            # verify ids from all tsx + local, find sprite ..
         if of : of.write(struct.pack('2h4B',0,0,255,0,0,0)) ; index +=1
-    if of : 
+    if of :
         of.seek(-8,2)
         of.write(struct.pack('2h4B',0,0,255,255,255,255)) ; index +=1
 
-    return objgroups, [tid2ts(tmap,oid,basefile) for oid in unique_oids]
+    return objgroups, unique_ts # ts name, tile type or None
 
 
-def out_layers(tmap,of=None,codec='u8') : 
+def out_layers(tmap,of=None,codec='u8') :
     index=0
     mw = int(tmap.get('width'))
     mh = int(tmap.get('height'))
     nb_layers = sum(1 for x in tmap.findall('layer') if not(x.get('name').startswith('_')))
-    if of : 
+    if of :
         array.array('H',(
             0xb17b,
             mw,mh,
-            MAP_FORMATS.index(codec)<<8 | nb_layers),            
+            MAP_FORMATS.index(codec)<<8 | nb_layers),
         ).tofile(of)
 
     min_indices=99999999
@@ -111,21 +120,21 @@ def out_layers(tmap,of=None,codec='u8') :
     for layer in tmap.findall('layer') :
         name=layer.get("name")
         if name[0]=='_' : continue # skip
-        
+
         lw = int(layer.get("width"))
         lh = int(layer.get("height"))
         out_code='H' if codec=='u16' else 'B'
 
         data = layer.find("data")
         if data.get('encoding')=='csv' :
-            indices = tuple(int(s) for s in data.text.replace("\n",'').split(','))                
+            indices = tuple(int(s) for s in data.text.replace("\n",'').split(','))
         elif data.get('encoding')=='base64' and data.get('compression')=='zlib' :
             indices = array.array('I',data.text.decode('base64').decode('zlib'))
         else :
             raise ValueError('Unsupported layer encoding :'+data.get('encoding'))
 
 
-        if codec=='u8' and max(indices)>=256 : 
+        if codec=='u8' and max(indices)>=256 :
             raise ValueError("size of index type too small : %d tiles used"%max(indices))
         assert len(indices) == lw*lh, "not enough or too much data : %d != %d"%(lw*lh, len(tidx))
 
@@ -141,7 +150,7 @@ def out_layers(tmap,of=None,codec='u8') :
 
     return max_indices, min_indices
 
-def read_tmap(file) : 
+def read_tmap(file) :
     print("showing info about map",file)
     f = open(file,'rb')
 
@@ -159,19 +168,19 @@ def read_tmap(file) :
         print("layer %d : "%i,arr[:20],'...')
     print(' ---')
     group = 0
-    while True : 
+    while True :
         x,y,uid,v,v2,v3 = struct.unpack('2h4B',f.read(8))
-        if uid==255 : 
+        if uid==255 :
             group+=1
             if v==255 : break # eof marker
-        else : 
+        else :
             print("object group:%d x:%3d y:%3d oid:%d value:%d"%(group,x,y,uid,v))
     print(' ---')
 
 
-if __name__=='__main__' : 
+if __name__=='__main__' :
     parser = argparse.ArgumentParser(description='Process files for bitbox graphical library.')
-    parser.add_argument('filename', help='input file (tmx)') 
+    parser.add_argument('filename', help='input file (tmx)')
     parser.add_argument('-f','--format',  help='format of the tilemap index (u8, u16)', default='u8')
 
     args = parser.parse_args()
@@ -182,7 +191,7 @@ if __name__=='__main__' :
 
     infile = open(args.filename,'r')
 
-    def usage(str) : 
+    def usage(str) :
         print(" Usage error :", str, file=sys.stderr)
         sys.exit(2)
 
@@ -201,10 +210,10 @@ if __name__=='__main__' :
 
     # write file references
     # reference (unique) to tileset
-    if m_idx>=0 : # any layer defined  
+    if m_idx>=0 : # any layer defined
         tsA, _ = tid2ts(tmap,m_idx,args.filename)
         tsB, _ = tid2ts(tmap,M_idx,args.filename)
-        if tsA != tsB : 
+        if tsA != tsB :
             # not really an error, just an info
             print(' stderr from several tilesets (%s, %s) are used for this tilemap. only first is included.'%(tsA,tsB), file=sys.stderr)
         print("#define %s_MAP  \"%s_map\""%(tmap_name, tmap_name))
@@ -212,13 +221,14 @@ if __name__=='__main__' :
     print()
 
     objgroups, unique_states = out_objects(tmap, args.filename, of)
-    if objgroups : 
+    if objgroups :
         print('#define %s_OBJECT_GROUPS \\'%tmap_name)
-        for name, index in objgroups : 
+        for name, index in objgroups :
             print("    X(%s,%s) \\"%(name,index))
-    if unique_states : 
+        print ()
+    if unique_states :
         print('#define %s_OBJECTS \\'%tmap_name)
-        for ts,typ in unique_states : 
+        for ts,typ in unique_states :
             if typ==None : usage('Referenced tile in %s has no type !'%ts)
             print("    X(%s,%s) \\"%(ts,typ))
-    print()
+        print()
